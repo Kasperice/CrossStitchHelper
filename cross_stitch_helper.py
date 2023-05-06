4da89b6ad96fb3000eaf5a8ec49656b013df3512
Kasper Biegun <kasper.biegun95@gmail.com>
2023-05-06 21:58:41 +0200

    Formatting fixed

--- cross_stitch_helper.py
+++ cross_stitch_helper.py
@@ -8,14 +8,19 @@ class CrossStitchHelper:
     def __init__(self, filename, config):
         self.config = config
         self.filename = filename.stem
-        self.template = cv2.imread(self.config.get('template', 'name'))
+        self.template = cv2.imread(self.config.get("template", "name"))
         self.scheme = self.open_and_resize_scheme()
         self.template_coordinates = self.create_template_coordinates()
 
     def create_thresholds_pickle_if_not_exists(self):
         if not os.path.exists("thresholds.p"):
             print("File thresholds.p not found, creating new one with default values.")
-            thresholds = {i: 0.60 for i in range(1, self.config.getint('template', 'number_of_colors') + 1)}
+            thresholds = {
+                i: 0.60
+                for i in range(
+                    1, self.config.getint("template", "number_of_colors") + 1
+                )
+            }
             pickle.dump(thresholds, open("thresholds.p", "wb"))
 
     def create_results_directory(self):
@@ -31,21 +36,23 @@ class CrossStitchHelper:
     @staticmethod
     def display_preview(image_to_show):
         cv2.namedWindow("image")
-        cv2.imshow('image', image_to_show)
+        cv2.imshow("image", image_to_show)
         cv2.waitKey(0)
-        cv2.destroyWindow('image')
+        cv2.destroyWindow("image")
         cv2.waitKey(1)
 
     def create_template_coordinates(self):
-        template_parameters = dict(self.config.items('template'))
+        template_parameters = dict(self.config.items("template"))
 
-        coords = [(x, y)
-                  for x in range(int(template_parameters['y']))
-                  for y in range(int(template_parameters['x']))][:int(template_parameters['number_of_colors'])]
+        coords = [
+            (x, y)
+            for x in range(int(template_parameters["y"]))
+            for y in range(int(template_parameters["x"]))
+        ][: int(template_parameters["number_of_colors"])]
 
-        x_offset = int(template_parameters['x_offset'])
-        y_offset = int(template_parameters['y_offset'])
-        sample_width = int(template_parameters['sample_width'])
+        x_offset = int(template_parameters["x_offset"])
+        y_offset = int(template_parameters["y_offset"])
+        sample_width = int(template_parameters["sample_width"])
 
         new_coords = []
         for x, y in coords:
@@ -57,25 +64,33 @@ class CrossStitchHelper:
 
     def open_and_resize_scheme(self):
         image = cv2.imread(f"{self.filename}.png")
-        scale_percent = 137 / self.config.getint('scheme', 'sample_width')
+        scale_percent = 137 / self.config.getint("scheme", "sample_width")
         new_width = int(image.shape[1] * scale_percent)
         new_height = int(image.shape[0] * scale_percent)
-        return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4)
+        return cv2.resize(
+            image, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4
+        )
 
     def find_colors_on_page(self):
         if not os.path.exists(f"{self.filename}_colors.p"):
-            print("File containing colors on this page was not found.\nNew will be created in a while, please wait...")
+            print(
+                "File containing colors on this page was not found.\nNew will be created in a while, please wait..."
+            )
             self.search_for_colors_on_page()
 
         return pickle.load(open(f"{self.filename}_colors.p", "rb"))
 
     def crop_template(self, coordinate):
-        cropped_template = self.template[coordinate[0][0]:coordinate[0][1], coordinate[1][0]:coordinate[1][1]]
+        cropped_template = self.template[
+            coordinate[0][0] : coordinate[0][1], coordinate[1][0] : coordinate[1][1]
+        ]
 
-        scale_percent = 110 / self.config.getint('template', 'sample_width')
+        scale_percent = 110 / self.config.getint("template", "sample_width")
         new_width = int(cropped_template.shape[1] * scale_percent)
         new_height = int(cropped_template.shape[0] * scale_percent)
-        return cv2.resize(cropped_template, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4)
+        return cv2.resize(
+            cropped_template, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4
+        )
 
     def search_for_colors_on_page(self):
         found = []
@@ -94,11 +109,12 @@ class CrossStitchHelper:
             if len(startX) == len(startY) != 0:
                 found.append(color_id)
 
-
         pickle.dump(found, open(f"{self.filename}_colors.p", "wb"))
 
     def prepare_scheme_for_color(self, color, custom_threshold=None):
-        for color_id, coordinates in enumerate(self.template_coordinates[color - 1:color], start=1):
+        for color_id, coordinates in enumerate(
+            self.template_coordinates[color - 1 : color], start=1
+        ):
             cropped_template = self.crop_template(coordinates)
 
             (tH, tW) = cropped_template.shape[:2]
@@ -117,8 +133,9 @@ class CrossStitchHelper:
 
             modified_scheme = self.scheme.copy()
 
-            for (x, y) in zip(found_x, found_y):
-                cv2.rectangle(modified_scheme, (x, y), (x + tW, y + tH),
-                              (0, 0, 255), 12)
+            for x, y in zip(found_x, found_y):
+                cv2.rectangle(
+                    modified_scheme, (x, y), (x + tW, y + tH), (0, 0, 255), 12
+                )
 
             return modified_scheme, threshold
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from configparser import ConfigParser
 from cross_stitch_helper import CrossStitchHelper
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config_file = ConfigParser()
-    config_file.read('config.ini')
+    config_file.read("config.ini")
 
-    filename = pathlib.Path(config_file.get('scheme', 'name'))
+    filename = pathlib.Path(config_file.get("scheme", "name"))
     if not os.path.isfile(filename):
         raise FileNotFoundError(f"Scheme file '{filename}' does not exist!")
 
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     main_menu_actions = [
         Option("Single color - enter single color number", "single"),
         Option("All - generate all images", "all"),
-        Option("Exit", "exit")
+        Option("Exit", "exit"),
     ]
 
     option, index = pick(main_menu_actions, "Choose your action:", indicator="\u25C6")
@@ -39,8 +39,10 @@ if __name__ == '__main__':
             new_scheme, _ = cross_stitch_helper.prepare_scheme_for_color(color=color)
             new_width = int(new_scheme.shape[1] * 0.2)
             new_height = int(new_scheme.shape[0] * 0.2)
-            clone = cv2.resize(new_scheme, (new_width, new_height), interpolation=cv2.INTER_AREA)
-            cv2.imwrite(f'{page_filename}/res_{color}_test.png', new_scheme)
+            clone = cv2.resize(
+                new_scheme, (new_width, new_height), interpolation=cv2.INTER_AREA
+            )
+            cv2.imwrite(f"{page_filename}/res_{color}_test.png", new_scheme)
 
     if option.value == "exit":
         print("Exiting without saving changes!")
@@ -49,10 +51,16 @@ if __name__ == '__main__':
         selected_color, _ = pick(["Custom", *colors_found], "Select color:")
         if selected_color == "Custom":
             selected_color = int(input("Please select color number: "))
-            if selected_color > cross_stitch_helper.config.getint('template', 'number_of_colors'):
-                print(f"Your image contains only {cross_stitch_helper.config.getint('template', 'number_of_colors')} "
-                      f"colors.")
-        new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(color=selected_color)
+            if selected_color > cross_stitch_helper.config.getint(
+                "template", "number_of_colors"
+            ):
+                print(
+                    f"Your image contains only {cross_stitch_helper.config.getint('template', 'number_of_colors')} "
+                    f"colors."
+                )
+        new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(
+            color=selected_color
+        )
         original_threshold = threshold
         new_threshold = threshold
 
@@ -65,28 +73,31 @@ if __name__ == '__main__':
             Option("Increase threshold (+0.030)", "++++"),
             Option("Decrease threshold (-0.030)", "----"),
             Option("Save - save changes and create file", "save"),
-            Option("Exit", "exit")
+            Option("Exit", "exit"),
         ]
 
         while True:
-            option, index = pick(single_color_menu_options,
-                                 f"Color: {selected_color} | Current threshold: {new_threshold}\nChoose your action:",
-                                 indicator="\u25C6")
+            option, index = pick(
+                single_color_menu_options,
+                f"Color: {selected_color} | Current threshold: {new_threshold}\nChoose your action:",
+                indicator="\u25C6",
+            )
 
             if "+" in option.value:
-                increase_value = option.value.count('+') * 0.0075
+                increase_value = option.value.count("+") * 0.0075
                 new_threshold = threshold + increase_value
                 print(f"Increasing threshold by {increase_value} to {new_threshold}")
-                new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(color=selected_color,
-                                                                                     custom_threshold=new_threshold)
+                new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(
+                    color=selected_color, custom_threshold=new_threshold
+                )
 
             if "-" in option.value:
-                decrease_value = option.value.count('-') * 0.0075
+                decrease_value = option.value.count("-") * 0.0075
                 new_threshold = threshold - decrease_value
                 print(f"Decreasing threshold by {decrease_value} to {new_threshold}")
-                new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(color=selected_color,
-                                                                                     custom_threshold=new_threshold)
-
+                new_scheme, threshold = cross_stitch_helper.prepare_scheme_for_color(
+                    color=selected_color, custom_threshold=new_threshold
+                )
 
             if option.value == "exit":
                 print("Exiting without saving changes!")
@@ -96,17 +107,23 @@ if __name__ == '__main__':
                 scale_percent = 20
                 new_width = int(new_scheme.shape[1] * 0.2)
                 new_height = int(new_scheme.shape[0] * 0.2)
-                new_scheme = cv2.resize(new_scheme, (new_width, new_height), interpolation=cv2.INTER_AREA)
+                new_scheme = cv2.resize(
+                    new_scheme, (new_width, new_height), interpolation=cv2.INTER_AREA
+                )
 
-                cv2.imwrite(f'{page_filename}/res_{selected_color}_test.png', new_scheme)
+                cv2.imwrite(
+                    f"{page_filename}/res_{selected_color}_test.png", new_scheme
+                )
                 print(f"File saved to: {page_filename}/res_{selected_color}_test.png")
                 if original_threshold != new_threshold:
-                    print(f"Overriding threshold value for color {selected_color} to {new_threshold}")
-                    cross_stitch_helper.override_threshold_for_color(color=selected_color, new_threshold=new_threshold)
+                    print(
+                        f"Overriding threshold value for color {selected_color} to {new_threshold}"
+                    )
+                    cross_stitch_helper.override_threshold_for_color(
+                        color=selected_color, new_threshold=new_threshold
+                    )
                 break
 
             if option.value == "preview":
                 print("Preview opened in a new window.\nPress any key to close it.")
                 cross_stitch_helper.display_preview(image_to_show=new_scheme)
-
-
